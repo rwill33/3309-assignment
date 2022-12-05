97248f3b521678bfd6e425c899e0dd534c1baf67
rwill33 <90706510+rwill33@users.noreply.github.com>
2022-12-04 20:48:29 -0500

    Format orders table

--- frontend/src/app/components/orders/orders.component.ts
+++ frontend/src/app/components/orders/orders.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Service } from 'src/app/services/service.service';
 
@@ -7,13 +8,18 @@ import { Service } from 'src/app/services/service.service';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
-
   constructor(public service: Service) { }
 
   async ngOnInit(): Promise<void> {
-    if (this.service.customerOrders.length == 0) {
-      this.service.customerOrders = await this.service.getCustomerOrders().toPromise();
-    }
+    let orders = await this.service.getCustomerOrders().toPromise();
+    orders.forEach((order:any) => {
+      const format = 'dd/MM/yyyy';
+      const myDate = order.datePlaced;
+      const locale = 'en-US';
+      order.datePlaced = formatDate(myDate, format, locale);
+      order.amount = `$${order.amount.toFixed(2)}`;
+    });
+    this.service.customerOrders = orders;
   }
 
 }
